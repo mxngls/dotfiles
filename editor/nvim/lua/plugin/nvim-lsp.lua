cce442d645806f6952a9fb46c82a0161ad0158f7
mxngls <Maximilian.e.hoenig@gmail.com>
2023-05-16 22:53:22 +0900

    Add borders for diagnostic and signatureHelp

--- editor/nvim/lua/plugin/nvim-lsp.lua
+++ editor/nvim/lua/plugin/nvim-lsp.lua
@@ -36,6 +36,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
       border = "rounded",
     })
 
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+      vim.lsp.handlers.signature_help,
+      { border = 'rounded' }
+    )
+
+    vim.diagnostic.config {
+      virtual_text = false,
+      float = { border = "rounded" },
+    }
+
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
