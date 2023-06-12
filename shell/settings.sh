96bba532c61ee3322d3bc9dbe44603504d917f37
mxngls <Maximilian.e.hoenig@gmail.com>
2023-06-13 07:53:35 +0900

    Make man pages more colorful
    Basically just some settings to make working with man pages in the terminal more pleasant.

---
+++ shell/settings.sh
@@ -0,0 +1,12 @@
+# Colorful man pages
+if [[ "$(tput colors)" == "256" ]]; then
+  colors256=' -DE210 -DS236.211  -DB210  -DC210 -DR253  -DN253  -Du+214 -Dk+253 -Dd+045 -DP253  -Ds+208 -Dk+069'
+  colors16='  -DEr   -DSb.r      -DBr    -DCr   -DRw    -DNw    -Du+y   -Dk+w   -Dd+0c  -DPw    -Ds+y   -Dkb'
+  less_options="-i -g -j5 -N --mouse --line-num-width 4 --use-color +Gg"
+  export MANPAGER="less $less_options $colors256 +Gg"
+else
+  export MANPAGER="less $less_options $colors16 +Gg"
+fi
+
+# Max length for man pages
+export MANWIDTH=$((COLUMNS > 80 ? COLUMNS - 10 : COLUMNS))
