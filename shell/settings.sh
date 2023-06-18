01608c0ae085ad4157db1f717a1f65259b9e6fdd
mxngls <Maximilian.e.hoenig@gmail.com>
2023-06-18 22:19:31 +0900

    Remove underline from Man pages (`less` options)

--- shell/settings.sh
+++ shell/settings.sh
@@ -1,7 +1,7 @@
 # Colorful man pages
 if [[ "$(tput colors)" == "256" ]]; then
-  colors256=' -DE210 -DS236.211  -DB210  -DC210 -DR253  -DN253  -Du+214 -Dk+253 -Dd+045 -DP253  -Ds+208 -Dk+069'
-  colors16='  -DEr   -DSb.r      -DBr    -DCr   -DRw    -DNw    -Du+y   -Dk+w   -Dd+0c  -DPw    -Ds+y   -Dkb'
+  colors256=' -DE210 -DS236.211  -DB210  -DC210 -DR253  -DN253  -Du214 -Dk+253 -Dd+045 -DP253  -Ds+208 -Dk+069'
+  colors16='  -DEr   -DSb.r      -DBr    -DCr   -DRw    -DNw    -Duy   -Dk+w   -Dd+0c  -DPw    -Ds+y   -Dk+b'
   less_options="-i -g -j5 -N --mouse --line-num-width 4 --use-color +Gg"
   export MANPAGER="less $less_options $colors256 +Gg"
 else
