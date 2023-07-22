34cf05ce0941e792b354896e37cc025ce0132ad1
mxngls <Maximilian.e.hoenig@gmail.com>
2023-07-22 15:39:41 +0900

    Enable sorting of output from ls
    Way more pleasant to digest the output in rows instead of columns.

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,8 +1,9 @@
 # Misc
 alias ep='echo $PATH | sed "s/:/\\n/g"'
 alias ls="ls        --color=always" 
-alias lsa="ls -aF   --color=always" 
-alias lsl="ls -lah  --color=always"
+alias lss="ls       --color=always -1 | sort -k 1.1,1.1 -t ." 
+alias lsa="ls -aF   --color=always    | sort -k 1.1,1.1 -t ." 
+alias lsl="ls -lh   --color=always    | sort -k 1.1,1.1 -t ."
 
 # Toggle theme
 alias ct="~/dotfiles/shell/toggle_theme.sh -t"
