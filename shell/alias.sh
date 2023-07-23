0874c46317c924feebe8c432785d003c495a4b9e
mxngls <Maximilian.e.hoenig@gmail.com>
2023-07-23 10:32:13 +0900

    Tweak aliases
    * Use single quotes to prevent immediate expansion.
    * Add '--color=always' only to the base ls alias.
    * Tweak ls aliases slightly.
    * Remove branch information from 'gs' alias as we already have this in
      our right prompt

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,17 +1,18 @@
 # Misc
-alias ep="echo $PATH | sed 's/:/\\n/g'"
-alias ls="ls        --color=always" 
-alias lss="ls       --color=always -1 | sort -k 1.1,1.1 -t ." 
-alias lsa="ls -aF   --color=always    | sort -k 1.1,1.1 -t ." 
-alias lsl="ls -lh   --color=always    | sort -k 1.1,1.1 -t ."
+alias ep='echo $PATH | sed "s/:/\\n/g"'
+alias ls='ls --color=always                   ' 
+alias lss='ls -F -1     | sort -k 1.1,1.1 -t .' 
+alias lsa='ls -aF       | sort -k 1.1,1.1 -t .' 
+alias lsl='ls -aFlh     | sort -k 1.1,1.1 -t .'
+alias me='print -P "%n@%m"'
 
 # Toggle theme
-alias ct="~/dotfiles/shell/toggle_theme.sh -t"
+alias ct='~/dotfiles/shell/toggle_theme.sh -t'
 
 # Quicker navigation
-alias gr="cd $(git rev-parse --show-toplevel)"
-alias gnv="cd ~/dotfiles/editor/nvim"
-alias gtx="cd ~/dotfiles/shell/tmux"
+alias gr='cd "${$(git rev-parse --git-dir 2>/dev/null)%.git}"'
+alias gnv='cd ~/dotfiles/editor/nvim'
+alias gtx='cd ~/dotfiles/shell/tmux'
 
 # Some Git stuff
-alias gs="git status -sb"
+alias gs='git status -s'
