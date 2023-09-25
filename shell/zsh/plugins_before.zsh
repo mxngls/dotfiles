f90bc1c78a36bc940f43cdcb8a76c96356723087
mxngls <Maximilian.e.hoenig@gmail.com>
2023-09-25 20:40:41 +0900

    Add Zsh custom completion script for _rename-stash

--- shell/zsh/plugins_before.zsh
+++ shell/zsh/plugins_before.zsh
@@ -5,6 +5,10 @@ if type brew &>/dev/null; then
   fpath=($(brew --prefix)/share/zsh-completions/src $fpath)
 fi
 
+# completions for docker
 if type docker &>/dev/null;then
   fpath=($(brew --prefix)/opt/docker-completion/share/zsh/site-functions/ $fpath)
 fi
+
+# custom completion functions
+fpath=(/Users/max/dotfiles/shell/zsh/completions/ $fpath)
