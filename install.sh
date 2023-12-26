72ee7876ccaf173b9bece04bc1acb72c73a5d810
mxngls <Maximilian.e.hoenig@gmail.com>
2023-12-26 22:05:46 +0900

    Rewrite install.sh

--- install.sh
+++ install.sh
@@ -1,95 +1,144 @@
 #!/bin/bash
 
-set -eu
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Dont link DS_Store files
 find . -name ".DS_Store" -exec rm {} \;
 
-# {{{ Backup current dotfiles
-function backup_dotfiles() {
-  echo "> Backing up current dotfiles..."
+# Constants
+DOTFILES_DIR="${HOME}/dotfiles"
+
+# Backup current dotfiles if they exist
+backup_dotfiles() {
 
   local backup_dir
-  backup_dir="dotfile_bk_$(date -u +"%Y%m%d%H%M%S")"
-  cd && mkdir -p "$backup_dir"
 
-  for el in "${HOME}/dotfiles"/*; do
-    if [ -f "$el" ] || [ -d "$el" ]; then
-      cp -R "$el" "$backup_dir"
+  echo "Backing up current dotfiles..."
+
+  local backup_dir="${DOTFILES_DIR}_bk_$(date -u +"%Y%m%d%H%M%S")"
+
+  if [[ ! -p $backup_dir ]]; then
+    mkdir -p "$backup_dir"
+
+    if [[ ! -d $DOTFILES_DIR ]]; then
+      echo 'No directory containing dotfiles found. Aborting backup process.'
+      return 0
     fi
-  done
 
-  echo -e "Done!\n"
+    for file in "${DOTFILES_DIR}"/*; do
+      if [ -f "$file" ] || [ -d "$file" ]; then
+        cp -R "$file" "$backup_dir"
+      fi
+    done
+
+    echo "Successfully created new backup of dotfiles directory."
+    return 0
+
+  else
+    echo 'Up to date backup already exists. Aborting backup process.'
+    return 1
+  fi
 }
 
-backup_dotfiles
-
-# }}}
-# {{{ Install homebrew
-function install_brew() {
+# Installing Homebrew if it does not exist yet
+install_brew() {
   if ! command -v brew &> /dev/null; then
-    echo "> Installing Homebrew..."
+    echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-   
-    echo -e "Done!\n"
+    echo 'Successfully installed Homebrew.'
   else
-    echo "> Homebrew is already installed"
+    echo 'Homebrew is already installed. Continuing trying to install packages.'
   fi
 
   # Install everything from Brewfile
-  echo "> Installing packages installed via Homebrew..."
-  brew bundle install --file=~/dotfiles/Brewfile
+  echo 'Installing packages installed via Homebrew...'
+  if [[ -f "${DOTFILES_DIR}/Brewfile" ]]; then
+    brew bundle install --file="${DOTFILES_DIR}/Brewfile"
+    echo "Successfully installed Homebrew packages."
+  else
+    echo 'No Brewfile found. Aborting installation of Homebrew packages.'
+  fi
 
-  echo -e "Done!\n"
+  return 0
 }
 
-install_brew
-
-# }}}
-# {{{ Linking
-function link_all() {
-  echo "> Linking files and directories"
-
-  cd "${HOME}"/dotfiles
+# Function to create a symbolic link if it doesn't exist
+link_file() {
+  local source_file="$1"
+  local target_file="$2"
   
-  # Shell
-  ln -sfn "$(pwd)"/shell/zsh/.zshenv            "${HOME}".zshenv
-  ln -sfn "$(pwd)"/shell/zsh/.zshrc             "${HOME}".zshrc
-  ln -sfn "$(pwd)"/shell/bash/.bashrc           "${HOME}".bashrc
-  ln -sfn "$(pwd)"/shell/bash/.bash_login       "${HOME}".bash_login
-  ln -sfn "$(pwd)"/shell/bash/.bash_logout      "${HOME}".bash_logout
-  ln -sfn "$(pwd)"/shell/bash/.bash_profile     "${HOME}".bash_profile
-  ln -sfn "$(pwd)"/shell/tmux                   "${HOME}".config
-  ln -sfn "$(pwd)"/shell/kitty                  "${HOME}".config
+  if [[ ! -e "$source_file" ]]; then
+    echo "Link failed: $target_file does not exist."
+    return 1
+  elif [[ ! -e "$target_file" ]]; then
+    ln -sfn "$source_file" "$target_file"
+    echo "Linked: $target_file."
+  else
+    echo "Skipped: $target_file already exists."
+    return 0
+  fi
+}
 
-  # Rest
-  ln -sfn "$(pwd)"/editor/nvim                  "${HOME}".config/
-  ln -sfn "$(pwd)"/.gitconfig                   "${HOME}".gitconfig
+# Function to link files and directories
+link_all() {
+  echo "Linking files and directories..."
 
-  for script in "$(pwd)"/shell/bin/*; do
-    ln -sfn "$script" "/usr/local/bin/$(basename "$script")"
+  # Shell related
+  link_file "${DOTFILES_DIR}/shell/zsh/.zshenv"          "${HOME}/.zshenv"
+  link_file "${DOTFILES_DIR}/shell/zsh/.zshrc"           "${HOME}/.zshrc"
+  link_file "${DOTFILES_DIR}/shell/bash/.bashrc"         "${HOME}/.bashrc"
+  link_file "${DOTFILES_DIR}/shell/bash/.bash_login"     "${HOME}/.bash_login"
+  link_file "${DOTFILES_DIR}/shell/bash/.bash_logout"    "${HOME}/.bash_logout"
+  link_file "${DOTFILES_DIR}/shell/bash/.bash_profile"   "${HOME}/.bash_profile"
+  link_file "${DOTFILES_DIR}/shell/tmux"                 "${HOME}/.config/tmux"
+  link_file "${DOTFILES_DIR}/shell/kitty"                "${HOME}/.config/kitty"
+
+  # Others
+  link_file "${DOTFILES_DIR}/editor/nvim" "${HOME}/.config/nvim"
+  link_file "${DOTFILES_DIR}/.gitconfig" "${HOME}/.gitconfig"
+
+  for script in "${DOTFILES_DIR}/shell/bin/"*; do
+    link_file "$script" "/usr/local/bin/$(basename "$script")"
   done
 
-  echo -e "Done!\n"
-  echo "> All files linked! Ready to go!"
+  echo 'Successfully linked all target files.'
 }
 
-link_all
+# Function to clone Git repos (Currently unused!)
+clone() {
+  local repo="$1"
 
-# }}}
-# {{{ Set up external repos that that can't be managed via Homebrew
-function get_other() {
+  # Check if the repository already exists
+  if [ ! -d "$repo" ]; then
+    echo "Cloning $repo."
+    git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+    echo "Successfully cloned $repo."
+  else
+    echo "Skipped: $repo already exists."
+  fi
+
+}
+
+# Function to clone external Git repositories
+clone_all() {
   cd "${HOME}"
-  mkdir -p Git && cd "$_"
-  git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+
+  echo 'Cloning Git repositories...'
+
+  if [[ ! -d ${HOME}/Git ]]; then
+    mkdir -p Git && cd Git
+    echo 'Created directory to store Git repositories (Git)'
+  fi
+
+  # Usage: clone REPO
+
+  return 0
 }
 
-# }}}
-# {{{ Set termial/vim theme
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  swiftc dark-mode-notify.swift -o dark-mode-notify
-  ln -sfn ~/dotfiles/shell/com.mxngls.dark-mode-notify.plist ~/Library/LaunchAgents/
-  launchctl load -w ~/Library/LaunchAgents/com.mxngls.dark-mode-notify.plist
-fi
-
-# }}}
+# Install
+backup_dotfiles
+install_brew
+link_file
+clone_all
