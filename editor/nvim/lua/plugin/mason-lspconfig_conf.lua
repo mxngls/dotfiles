3937e3360794a02586bcd96c2b4c1be520db013d
mxngls <Maximilian.e.hoenig@gmail.com>
2024-08-16 22:34:15 +0900

    Remove unused language servers

--- editor/nvim/lua/plugin/mason-lspconfig_conf.lua
+++ editor/nvim/lua/plugin/mason-lspconfig_conf.lua
@@ -2,10 +2,7 @@ require('mason').setup()
 require('mason-lspconfig').setup({
   ensure_installed = {
     'bashls',
-    'cssls',
-    'html',
     'lua_ls',
-    'marksman',
     'pyright',
     'sqlls',
     'tailwindcss',
