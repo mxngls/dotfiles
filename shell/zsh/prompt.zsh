f60f89067e92391aab4b54a3f201f80bda75bdc3
mxngls <Maximilian.e.hoenig@gmail.com>
2024-08-30 23:46:16 +0900

    Extract script to fetch repo meta info

--- shell/zsh/prompt.zsh
+++ shell/zsh/prompt.zsh
@@ -1,59 +1,20 @@
+#!/bin/zsh
+
 # Enable prompt substitution
 setopt PROMPT_SUBST
-
-git_info() {
-
-    git_oid=""
-    git_head=""
-    git_upstream=""
-    git_ahead=""
-    git_behind=""
-
-    oid_reg='# branch\.oid ([a-z0-9]+)'
-    head_reg='# branch\.head (.+)'
-    upstream_reg='# branch\.upstream (.+)'
-    ahead_reg='# branch\.ab (\+[1-9]+) .*'
-    behind_reg='# branch\.ab .* (\-[1-9]+)'
-
-    while read -r; do
-        if [[ "$REPLY" =~ $oid_reg ]]; then
-            git_oid="${BASH_REMATCH[1]}"
-        elif [[ "$REPLY" =~ $head_reg ]]; then
-            git_head="${BASH_REMATCH[1]}"
-        elif [[ "$REPLY" =~ $upstream_reg ]]; then
-            git_upstream="${BASH_REMATCH[1]}"
-        elif [[ "$REPLY" =~ $ahead_reg || "$REPLY" =~ $behind_reg ]]; then
-            git_ahead="${BASH_REMATCH[1]}"
-            git_behind="${BASH_REMATCH[2]}"
-        fi
-    done < <(git \
-        --no-optional-locks \
-        status \
-        --porcelain=v2 \
-        --branch)
-
-    printf "%s" "[ "
-
-    printf "%s %s" "${git_oid:0:7}" "$git_head"
-
-    [[ -n "$git_ahead" ]] && printf " %s " "$git_ahead"
-    [[ -n "$git_behind" ]] && printf " %s " "$git_behind"
-
-    printf "%s" " ]"
-}
-
+autoload -U colors && colors
 
 # Prompt segments
+time_segment() {
+    echo "[%F{cyan}%D{%I:%M}%f]"
+}
+
 user_host_segment() {
-    echo "[ %n@%m ]"
+    echo "[%F{magenta}%n@%m%f]"
 }
 
 directory_segment() {
-    echo "[ %~ ]"
-}
-
-time_segment() {
-    echo "[ %D{%I:%M} ]"
+    echo "[%F{green}%~%f]"
 }
 
 prompt_line() {
@@ -63,12 +24,18 @@ prompt_line() {
 GIT_INFO=''
 
 # Construct prompt
-PROMPT='$(prompt_line "┌─")$(directory_segment) - $(time_segment) - $(user_host_segment) ${GIT_INFO} 
+PROMPT='$(prompt_line "┌─") $(time_segment)-$(user_host_segment)-$(directory_segment)${GIT_INFO}
 $(prompt_line "└─")> '
 
 PS2='$(prompt_line ">")'
 
 function precmd() {
+    if [[ ! "$(command -v git_info)" ]];then
+        echo "Script to fetch basic information about the current repo and it's state failed."
+        echo "Aborting."
+        return
+    fi
+
     typeset -g PROMPT_FD
 
     # close last fd if it exists
