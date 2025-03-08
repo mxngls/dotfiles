d3d0754b63cdbd4da96f46feb028f88b5b67ad86
mxngls <Maximilian.e.hoenig@gmail.com>
2025-03-08 14:21:38 +0900

    Properly setup Conform

--- editor/nvim/lua/init.lua
+++ editor/nvim/lua/init.lua
@@ -5,6 +5,7 @@ require('plugin/mason')
 require('plugin/nvim-lsp')
 require('plugin/nvim-cmp')
 require('plugin/telescope')
+require('plugin/conform')
 
 -- }}}
 
--- editor/nvim/lua/plugin/conform.lua
+++ editor/nvim/lua/plugin/conform.lua
@@ -1,6 +1,9 @@
 require("conform").setup({
     formatters_by_ft = {
+        lua = { "stylua" },
         javascript = { "prettier" },
+        typescript = { "prettier" },
+        c = { "clang-format" }
     },
     format_on_save = {
         timeout_ms = 500,
