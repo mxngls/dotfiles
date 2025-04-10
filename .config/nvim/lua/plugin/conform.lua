394f053655a9b477d641d561809f042de84e83ac
mxngls <Maximilian.e.hoenig@gmail.com>
2025-04-10 10:00:08 +0900

    Format conform.lua

--- .config/nvim/lua/plugin/conform.lua
+++ .config/nvim/lua/plugin/conform.lua
@@ -1,12 +1,17 @@
 require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        c = { "clang-format" }
-    },
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-    },
+	formatters = {
+		prettier = {
+			require_cwd = true,
+		},
+	},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		c = { "clang-format" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
 })
