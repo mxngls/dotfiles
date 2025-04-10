69191d2fb78db83a6f7cfd76cf4540462f06cb2e
mxngls <Maximilian.e.hoenig@gmail.com>
2025-04-10 09:50:30 +0900

    Navigate Zsh's Complist through home-row

--- .config/zsh/bindkey.sh
+++ .config/zsh/bindkey.sh
@@ -17,3 +17,10 @@ bindkey '^N' history-search-forward
 # backspace
 bindkey '^?' backward-delete-char
 bindkey '^[[3~' delete-char
+
+# completion menu
+zmodload zsh/complist
+bindkey -M menuselect 'h' vi-backward-char      
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
