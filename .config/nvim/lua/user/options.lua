d1c556ea23af20d4c41b1160fa1380a0e49a0fa9
mxngls <Maximilian.e.hoenig@gmail.com>
2025-07-16 15:28:17 +0900

    Some more vim options

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -14,21 +14,29 @@ vim.o.smartcase = true
 
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+vim.o.smarttab = true
+vim.o.expandtab = false
 
 vim.o.inccommand = "split"
 
 vim.o.foldenable = false
 vim.o.foldmethod = "indent"
+
+vim.opt.diffopt:append({ "iwhite" })
+
 -- new buffers
 vim.o.splitbelow = true
 vim.o.splitright = true
 
 -- ui
 vim.o.termguicolors = false
+
 vim.o.textwidth = 100
+vim.o.wrap = false
 
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
 vim.o.colorcolumn = "100"
 vim.o.cursorline = true
 
