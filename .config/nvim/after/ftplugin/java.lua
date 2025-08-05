d7215765df8df464c37514fb20dbb33ea97a5d3c
Max <max@dencomm.ai>
2025-08-05 10:54:11 +0900

    Fix jdtls setup

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -4,8 +4,25 @@ local root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upw
 -- jdtls
 require("jdtls").start_or_attach({
 	cmd = {
-		"/Users/max/.local/share/nvim/mason/bin/jdtls",
-		("--jvm-arg=-javaagent:%s"):format(vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar")),
+		vim.fn.system("/usr/libexec/java_home -v 21"):gsub("\n", "") .. "/bin/java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens=java.base/java.util=ALL-UNNAMED",
+		"--add-opens=java.base/java.lang=ALL-UNNAMED",
+		("-javaagent:%s"):format(vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar")),
+		"-jar",
+		vim.fn.glob(
+			vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")
+		),
+		"-configuration",
+		vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/config_mac"),
+		"-data",
+		vim.fn.expand("~/.cache/jdtls/workspace") .. root_dir,
 	},
 	root_dir = root_dir,
 	settings = {
@@ -14,7 +31,7 @@ require("jdtls").start_or_attach({
 				runtimes = {
 					{
 						name = "JavaSE-21",
-						path = "/opt/homebrew/Cellar/openjdk@21/21.0.7/libexec/openjdk.jdk/Contents/Home",
+						path = vim.fn.system("/usr/libexec/java_home -v 21"):gsub("\n", ""),
 						default = true,
 					},
 				},
