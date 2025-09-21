b85b8fb1ba52b94de132e705ce8229fbd7a7180d
mxngls <Maximilian.e.hoenig@gmail.com>
2025-09-21 23:47:05 +0900

    Highlight on yank

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -31,3 +31,9 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.bo.spelllang = "en_us"
 	end,
 })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.hl.on_yank({ timeout = 150 })
+	end,
+})
