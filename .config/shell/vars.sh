1f113cedfd411de96eab103f57fc629bbddf32fb
mxngls <Maximilian.e.hoenig@gmail.com>
2025-09-21 23:47:45 +0900

    Export cargo and rustup environment variables

--- .config/shell/vars.sh
+++ .config/shell/vars.sh
@@ -5,7 +5,13 @@ if command -v rg >/dev/null 2>&1; then
 fi
 
 if command -v colima >/dev/null 2>&1 && command -v jq >/dev/null 2>&1; then
-    export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
-    export TESTCONTAINERS_HOST_OVERRIDE=$(colima ls -j | jq -r '.address')
-    export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
+	export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
+	export TESTCONTAINERS_HOST_OVERRIDE=$(colima ls -j | jq -r '.address')
+	export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
+fi
+
+if command -v cargo >/dev/null 2>&1; then
+	export CARGO_HOME="$HOME/.local/share/cargo"
+	export RUSTUP_HOME="$HOME/.local/share/rustup"
+	export PATH="$CARGO_HOME/bin:$PATH"
 fi
