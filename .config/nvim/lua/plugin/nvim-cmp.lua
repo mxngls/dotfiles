cd0d68337bf4368b70f56639bb0038f81fb437b5
mxngls <Maximilian.e.hoenig@gmail.com>
2025-09-21 23:46:26 +0900

    Remove nvim-cmp throttle and debounce

--- .config/nvim/lua/plugin/nvim-cmp.lua
+++ .config/nvim/lua/plugin/nvim-cmp.lua
@@ -9,12 +9,6 @@ cmp.setup({
 		end,
 	},
 
-	-- Performance settings
-	performance = {
-		-- debounce = 800,
-		-- throttle = 100,
-	},
-
 	-- Completion sources
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
