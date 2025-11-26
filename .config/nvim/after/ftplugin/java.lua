a996e5a3500fd4ffc5fb91697065666f820ddb09
mxngls <Maximilian.e.hoenig@gmail.com>
2025-11-26 10:25:16 +0900

    Java setup
    # Conflicts:
    #	.config/nvim/lua/plug.lua

---
+++ .config/nvim/after/ftplugin/java.lua
@@ -0,0 +1,49 @@
+-- shared
+local root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1])
+
+-- jdtls
+require("jdtls").start_or_attach({
+	cmd = {
+		vim.fn.system("/usr/libexec/java_home -v 21"):gsub("\n", "") .. "/bin/java",
+		("-javaagent:%s"):format(vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar")),
+		"-jar",
+		vim.fn.glob(
+			vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")
+		),
+		"-configuration",
+		vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/config_mac"),
+		"-data",
+		vim.fn.expand("~/.cache/jdtls/workspace") .. root_dir,
+	},
+	root_dir = root_dir,
+	settings = {
+		java = {
+			configuration = {
+				runtimes = {
+					{
+						name = "JavaSE-21",
+						path = vim.fn.system("/usr/libexec/java_home -v 21"):gsub("\n", ""),
+						default = true,
+					},
+				},
+			},
+			format = {
+				enabled = false,
+			},
+		},
+	},
+})
+
+-- checkstyle
+local checkstyle_config = root_dir and vim.fn.expand(root_dir .. "/config/checkstyle/checkstyle.xml") or nil
+require("lint").linters_by_ft = { java = { "checkstyle" } }
+require("lint").linters.checkstyle.config_file = checkstyle_config or "/google_checks.xml"
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})
+
+-- spaces instead of tabs
+vim.bo.expandtab = true
