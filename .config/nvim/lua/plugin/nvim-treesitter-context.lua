820aef7e8585624a7a49e69c9b399951849344ec
mxngls <Maximilian.e.hoenig@gmail.com>
2026-01-16 14:33:57 +0900

    Setup treesitter-context

--- .config/nvim/lua/plugin/nvim-treesitter-context.lua
+++ .config/nvim/lua/plugin/nvim-treesitter-context.lua
@@ -1,4 +1,13 @@
-require 'treesitter-context'.setup {
+local ts_context = require("treesitter-context")
+
+ts_context.setup({
 	multiwindow = true,
-	max_lines = 1,
-}
+	max_lines = 2,
+	multiline_threshold = 1,
+	trim_scope = "inner",
+})
+
+-- Jump to context
+vim.keymap.set("n", "<leader>c", function()
+	ts_context.go_to_context(vim.v.count1)
+end, { silent = true })
